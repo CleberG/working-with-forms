15377522d6279082c61c5e2b2ef70ef4c98c0d3c
Cataline Meneghelli <cataline.meneghelli@gmail.com>
2022-09-07 11:28:57 -0300

    adicioando validaçao para nao incluir dados vazio ou membro repetido

--- scripts.js
+++ scripts.js
@@ -1,24 +1,48 @@
 members = [
-    {
-        fname: 'John',
-        lname: 'Lennon',
-        instrument: 'Violão'
-    },
-    {
-        fname: 'George',
-        lname: 'Harrison',
-        instrument: 'Guitarra'
-    }
+  {
+    fname: 'John',
+    lname: 'Lennon',
+    instrument: 'Violão'
+  },
+  {
+    fname: 'George',
+    lname: 'Harrison',
+    instrument: 'Guitarra'
+  }
 ]
 
 const handlingForms = {
-
-    data() {
-        return {
-            members: window.members
+  data() {
+    return {
+      members: window.members,
+      newMember: {}
+    }
+  },
+  methods: {
+    addNewMembers: function () {
+      if (
+        this.newMember.fname &&
+        this.newMember.lname &&
+        this.newMember.instrument
+      ) {
+        console.log(
+          members.findIndex(member => member.fname === this.newMember.fname)
+        )
+        if (
+          members.findIndex(member => member.fname === this.newMember.fname) < 0 &&
+          members.findIndex(member => member.lname === this.newMember.lname) < 0 && 
+        ) {
+          this.members.push(this.newMember)
+          this.newMember = {}
+        } else {
+          alert('você não pode adicionar o mesmo membro mais de uma vez')
+          this.newMember = {}
         }
-    },
+      } else {
+        alert('Você não pode adicionar com dados vazio')
+      }
+    }
+  }
+}
 
-};
-
-Vue.createApp(handlingForms).mount('#app');
+Vue.createApp(handlingForms).mount('#app')
